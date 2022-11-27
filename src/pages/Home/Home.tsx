a08b12999d300e73f205a35afcd0f971af7264b0
Aileen Ma <ma.aileen29@gmail.com>
2022-11-27 04:36:51 -0500

    stackblitz migration v2

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <p>
+      The Asian-Pacific Association of Longmont (A-PAL) is a 501(c)(3)
+      organization, founded in 2010. A-PAL’s mission is to promote diversity and
+      bridge cultural gaps through the development and hosting of cultural,
+      educational, and artistic projects and events, as well as by encouraging
+      active participation in city and school district initiatives and
+      activities. A-PAL also urges greater utilization of programs and services
+      offered by public institutions. Additionally, A-PAL provides referral
+      assistance to those who have the need or are unfamiliar with available
+      resources.
+    </p>
+  );
+};
