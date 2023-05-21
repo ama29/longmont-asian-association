2866931a01b86b15b506d4132c61c36a2f189168
ama29 <ma.aileen29@gmail.com>
2023-05-21 15:17:01 +0000

    finished draft

---
+++ src/lib/Gallery2023.tsx
@@ -0,0 +1,53 @@
+import Gallery from "react-photo-gallery";
+const images = [
+    {
+        src: require("../images/events/gallery/2023/2023-front.png"),
+        width: 3,
+        height: 2
+    },
+    {
+        src: require("../images/events/gallery/2023/2023-back.png"),
+        width: 3,
+        height: 2
+    },
+    {
+        src: require("../images/events/gallery/2023/lion-dance-1.jpg"),
+        width: 3,
+        height: 2
+    },
+    {
+        src: require("../images/events/gallery/2023/drums-1.jpg"),
+        width: 3,
+        height: 2
+    },
+    {
+        src: require("../images/events/gallery/2023/dance-1.jpg"),
+        width: 3,
+        height: 2
+    },
+    {
+        src: require( "../images/events/gallery/2023/qipao-show-1.jpg"),
+        width: 3,
+        height: 2
+    },
+    {
+        src: require( "../images/events/gallery/2023/calliagraphy-1.jpg"),
+        width: 3,
+        height: 2
+    },
+    {
+        src: require( "../images/events/gallery/2023/qipao2.jpeg"),
+        width: 3,
+        height: 2
+    },
+]
+
+const Gallery2018 = () => {
+    return (
+        <div>
+            <Gallery photos={images}/>
+        </div>
+    );
+};
+
+export default Gallery2018;
