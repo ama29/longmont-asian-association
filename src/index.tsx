5f0f7b24f67da69f01568431355b1bff8a8def87
ama29 <ma.aileen29@gmail.com>
2023-05-22 10:36:45 +0000

    random link found its way in

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
